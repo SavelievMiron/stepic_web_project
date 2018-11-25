3d52a78b10eb5b2b20fe7e71e917ecfd0631f29b
SavelievMiron <44551982+SavelievMiron@users.noreply.github.com>
2018-11-25 16:23:36 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def application(environ, start_response):
-  start_response('200 OK',[('Content-type','text/plain')])
+  start_response('200 OK',[('Content-Type','text/plain')])
   start_response(status, response_headers)
   resp = environ['QUERY_STRING'].split("&")
   resp = [item+"\r\n" for item in resp]
