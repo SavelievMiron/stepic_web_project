45cae1afe754495c5b7c88c26775353ced35a1dd
SavelievMiron <44551982+SavelievMiron@users.noreply.github.com>
2018-11-25 20:05:29 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,7 @@
-#def app(environ, start_response):
-#  start_response('200 OK',[('Content-Type','text/plain')])
-#  start_response(status, response_headers)
-#  body = [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
-#  return body
-
 def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),encoding="utf8")]
+  start_response('200 OK',[('Content-Type','text/plain')])
+  start_response(status, response_headers)
+  body = [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
+  return body
+
+
